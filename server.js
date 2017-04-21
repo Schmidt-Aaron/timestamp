4304ae4e3a6c00fd7d8c5f9e6fd9100e5c93c108
Schmidt-Aaron <aschmidt808@gmail.com>
2017-04-21 13:42:56 -0700

    basic functionality added

--- server.js
+++ server.js
@@ -2,20 +2,33 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http');
+var moment = require('moment');
 
 //sets folder to be used for static elements /sets homepage
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.all('*', function(req, res, next) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    next();
-});
+// app.all('*', function(req, res, next) {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     next();
+// });
 
 //does stuff with the added strings
 app.get('/:str', function(req, res) {
     //captured parameter
     var str = req.params.str;
-    res.send('string:' + str + '\n');
+    var finalDate = {
+        "unix" : null,
+        "natural" : null
+    }
+    if(Number(str) >= 0) {
+        finalDate.unix = Number(str);
+        finalDate.natural = moment.unix(Number(date)).format("MMMM D, YYYY")
+    }
+    if(isNaN(Number(str)) && moment(str, "MMM D, YYYY").isValid()) {
+        finalDate.unix = moment(str, "MMMM D, YYYY").format("X");
+        finalDate.natural = str;
+    }
+    res.send(finalDate);
     //res.send({ 'unix': unix + 'natural:'+ natural  });
     console.log(str);
     
@@ -30,9 +43,11 @@ function check(date){
 }
 
 //converts the string
-function convert(date); 
-
-//inits server at port 3000
-http.createServer(app).listen(3000, function(){
-    console.log("server is running at 3000 port")
+function convert(date) {} 
+//creates server and set it to listen
+http.createServer(app);
+//var port = process.env.PORT ||'3000';
+//app.set('port', port)
+app.listen(3000, function(){
+    console.log("server is running at localhost: ${port}")
 });
