6495a235e6845ae4eb1035978a5929749ab64e56
Schmidt-Aaron <aschmidt808@gmail.com>
2017-04-21 11:44:58 -0700

    added basic app files

---
+++ public/js/functions.js
@@ -0,0 +1,2 @@
+//put required js here
+document.querySelector('.js').innerHTML ="Js file is linked, hoorah";
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-})
+//sets folder to be used for static elements /sets homepage
+app.use(express.static(path.join(__dirname, '/public')));
+
+//does stuff with the added strings
+app.get('/:str', function(req, res) {
+    //captured parameter
+    var str = req.params.str;
+    res.send('string:' + str + '\n');
+    //res.send({ 'unix': unix + 'natural:'+ natural  });
+    console.log(str);
+    
+    // var date = new Date().toLocaleDateString();
+    // res.send(date);
+
+
+});
+//checks to see if the string is a date
+function check(date){
+
+}
+
+//converts the string
+function convert(date); 
+
+//inits server at port 3000
+app.listen(3000, function(){
+    console.log("server is running at 3000 port")
+});
